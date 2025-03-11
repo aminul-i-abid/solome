251bb0eebe376d61b6621e3b5fb74cc9a641c7f9
aminulislamabid <aminul.i.abid@gmail.com>
2025-03-11 14:11:32 +0600

    feat(auth): register route implement, add: csrf module, token util

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,27 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  lastName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  password: string;
+}
